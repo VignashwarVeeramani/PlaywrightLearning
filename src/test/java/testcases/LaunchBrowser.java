85a90498a6ba35efd4e638ebd304ab50e8b61097
Vignashwar Veeramani <vignashwarv@gmail.com>
2024-04-01 11:08:10 +1100

    Maximize the browser dynamically

--- src/test/java/testcases/LaunchBrowser.java
+++ src/test/java/testcases/LaunchBrowser.java
@@ -1,9 +1,8 @@
 package testcases;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+
+import java.awt.*;
 
 public class LaunchBrowser {
     public static void main(String[] args) throws InterruptedException {
@@ -12,6 +11,7 @@ public class LaunchBrowser {
         Playwright playwright = Playwright.create(); //launch websocket session
 
         //Test case 1 - Headless mode
+        System.out.println("----------Test case 1 - Headless mode-------------- ");
         Browser browser = playwright.chromium().launch();
         Page page = browser.newPage();
         page.navigate("https://staging-www.forevernew.com.au/georgia-petite-high-waist-pants-257760?colour=black");
@@ -19,12 +19,30 @@ public class LaunchBrowser {
         page.close();
 
         //Test case 2 - With head mode
+        System.out.println("----------Test case 2 - With head mode-------------- ");
         Browser browser1 = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page1 = browser1.newPage();
         page1.navigate("https://staging-www.forevernew.com.au/georgia-petite-high-waist-pants-257760?colour=black");
         Thread.sleep(2000); //never use - learning purpose only
         System.out.println(page1.title());
         page1.close();
+
+        //Test case 3 - Maximise the window
+        System.out.println("----------Test case 3 - Maximise the window-------------- ");
+
+        //To dynamically pick the size of the working screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        System.out.println(width+"---"+height);
+
+        Browser browser2 = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext browserContext =  browser2.newContext(new Browser.NewContextOptions().setViewportSize((int)width,(int)height));
+        Page page2 = browserContext.newPage();
+        page2.navigate("https://staging-www.forevernew.com.au/georgia-petite-high-waist-pants-257760?colour=black");
+        Thread.sleep(2000); //never use - learning purpose only
+        System.out.println(page2.title());
+        page2.close();
 
         playwright.close(); //close websocket session
     }
